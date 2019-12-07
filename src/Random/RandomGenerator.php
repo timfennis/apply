b1e1ba9b3a28950f1276790fa45e293160558865
Tim Fennis <tim.fennis@nos.nl>
2019-12-07 13:53:46 +0100

    Moved Twister.php to a separate file to appease phpstan

--- src/Random/RandomGenerator.php
+++ src/Random/RandomGenerator.php
@@ -4,51 +4,6 @@ declare(strict_types=1);
 
 namespace Apply\Random;
 
-/**
- * @internal
- */
-class Twister
-{
-    /** @var array */
-    private array $state;
-
-    private function __construct(array $state)
-    {
-        $this->state = $state;
-    }
-
-    public function get(int $i)
-    {
-        return $this->state[$i];
-    }
-
-    public static function fromSeed($seed): Twister
-    {
-        $state[0] = $seed & 0xffffffff;
-
-        for ($i = 1; $i < 624; ++$i) {
-            $state[$i] = (((0x6c078965 * ($state[$i - 1] ^ ($state[$i - 1] >> 30))) + $i)) & 0xffffffff;
-        }
-
-        return new self($state);
-    }
-
-    public static function generate(Twister $twister): Twister
-    {
-        $state = $twister->state;
-        for ($i = 0; $i < 624; ++$i) {
-            $y = (($state[$i] & 0x1) + ($state[$i] & 0x7fffffff)) & 0xffffffff;
-            $state[$i] = ($state[($i + 397) % 624] ^ ($y >> 1)) & 0xffffffff;
-
-            if (1 == ($y % 2)) {
-                $state[$i] = ($state[$i] ^ 0x9908b0df) & 0xffffffff;
-            }
-        }
-
-        return new self($state);
-    }
-}
-
 class RandomGenerator
 {
     private Twister $twister;
