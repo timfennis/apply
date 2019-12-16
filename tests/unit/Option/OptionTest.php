496d61835efce98cbe9732f2291cbf2f66b44e99
Tim Fennis <tim.fennis@nos.nl>
2019-12-16 09:55:12 +0100

    added some tests for Option

--- tests/unit/Option/OptionTest.php
+++ tests/unit/Option/OptionTest.php
@@ -8,9 +8,45 @@ use Apply\Option\None;
 use Apply\Option\Option;
 use Apply\Option\Some;
 use Codeception\Test\Unit;
+use function Apply\Option\just;
 
 class OptionTest extends Unit
 {
+    public function testFilter()
+    {
+        $option = just(10);
+        $this->assertInstanceOf(Some::class, $option->filter(fn($v) => $v > 5));
+        $this->assertInstanceOf(None::class, $option->filter(fn($v) => $v > 15));
+    }
+
+    public function testMap()
+    {
+        $option = just(5);
+
+        $this->assertInstanceOf(Some::class, $option->map(fn ($v) => $v + 3));
+        $this->assertEquals(8, $option->map(fn ($v) => $v + 3)->orNull());
+
+        // Assert that the lambda is not called for None
+        $this->assertInstanceOf(None::class, None::create()->map(fn($x) => $this->fail('This lambda should not be called')));
+    }
+
+    public function testFlatMap()
+    {
+        $this->assertInstanceOf(None::class, None::create()->flatMap(fn() => just(10)));
+        $this->assertInstanceOf(None::class, just(5)->flatMap(fn() => None::create()));
+        $this->assertInstanceOf(Some::class, just(5)->flatMap(fn($five) => just($five + 3)));
+    }
+
+    /**
+     * @dataProvider invalidReturnValueProvider
+     */
+    public function testThatInvalidTypesCannotBeReturnedFromLambdaPassedToFlatMap($invalidReturnValue)
+    {
+        $this->expectException(\Apply\Exception\InvalidStateException::class);
+        just(5)->flatMap(fn() => $invalidReturnValue);
+
+    }
+
     public function testOptionBindingSuccess()
     {
         $result = Option::binding(static function () {
@@ -52,6 +88,17 @@ class OptionTest extends Unit
             [None::create(), Some::fromValue(1), None::create()],
             [Some::fromValue(1), None::create(), None::create()],
             [None::create(), None::create(), None::create()],
+        ];
+    }
+
+    public function invalidReturnValueProvider()
+    {
+        return [
+            [1],
+            ['string'],
+            [new \stdClass()],
+            [1.1],
+            [false],
         ];
     }
 }
